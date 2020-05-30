255a46705817d0aa034dba5a0b7455219eac3715
Andrii22 <avmelnyk.developer@gmail.com>
2020-05-30 19:18:13 +0300

    add new point for my homework

---
+++ boolean.py
@@ -0,0 +1,12 @@
+def bool_to_word(boolean):
+    if boolean == True:
+      return('Yes')
+    elif boolean == False:
+      return('No')
+    #if True:
+     # return('Yes')
+    #elif False:
+     #  return('No')
+    #else:
+     #  print('Try again')
+      
